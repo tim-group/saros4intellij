254d890131b0c05f0db90667f268380fd10414ea
Grundlefleck <grundlefleck@gmail.com>
2013-10-10 15:37:16 +0100

    GrahamA: start on specified port.

--- proxy/src/main/java/com/timgroup/saros4intellij/proxy/Main.java
+++ proxy/src/main/java/com/timgroup/saros4intellij/proxy/Main.java
@@ -5,6 +5,6 @@ import com.timgroup.saros4intellij.proxy.server.RestService;
 
 public class Main {
     public static void main(String[] args) {
-        new RestService(new SysoutNavigator(), new SysoutEditor()).start();
+        new RestService(7374, new SysoutNavigator(), new SysoutEditor()).start();
     }
 }
